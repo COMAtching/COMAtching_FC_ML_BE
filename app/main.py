e7cb4aa46fa06953b2b79a8921cbf7199509bd2c
oxqnd <kkinho04@catholic.ac.kr>
2024-10-05 22:18:23 +0900

    [UPDATE] reserve auth consumer

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from app import app
 from app.routes import users, recommend
-from app.consumers import match_request_consumer, user_crud_consumer
+from app.consumers import match_request_consumer, user_crud_consumer, reserve_auth_consumer
 import asyncio
 
 @app.on_event("startup")
 async def startup_event():
     asyncio.create_task(match_request_consumer.consume_from_queue())
     asyncio.create_task(user_crud_consumer.consume_user_crud_request_queue())
-#test
+    asyncio.create_task(reserve_auth_consumer.consume_reserve_auth_queue())
 
 # 라우터 등록
 app.include_router(users.router)
