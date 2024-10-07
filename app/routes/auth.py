ab60b56d97816a797dd5697c1cce350930c8ca44
jang_jiwon <kooriangman@gmail.com>
2024-10-07 17:17:55 +0900

    [ADD] 구단 구별 로직 추가

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -15,30 +15,43 @@ async def check_reserve_number(data: dict):
     
     # props가 데이터에 포함되어 있는지 확인
     props = data.get('props')
-
-    # 만약 props에 reply_to나 correlation_id가 없으면 오류 반환
-    if not props or not props.get('reply_to') or not props.get('correlation_id'):
-        return JSONResponse(content={"error": "Missing properties (reply_to or correlation_id)"}, status_code=400)
-    
     reserve_number = data["reserveNumber"]
     
-    if reserve_number in ticket_list['대표티켓번호'].values:
-        response_content = { "authSuccess": True }
+    # 만약 props에 reply_to나 correlation_id가 없으면 오류 반환
+    if not props or not props.get('reply_to') or not props.get('correlation_id'):
+        response_content = { "stateCode": "GEN-001", "authSuccess": False, "teamSide": "NONE" }
+        response_content.update(data)
+        print(f"Response: {response_content}")
+        await send_to_queue(None, props, response_content)
+        return JSONResponse(content=response_content, status_code=400)
+    
+    
+    if reserve_number in ticket_list_home['대표티켓번호'].values:
+        response_content = { "stateCode": "GEN-000", "authSuccess": True, "teamSide": "HOME" }
         response_content.update(data)
         print(f"Response: {response_content}")
         await send_to_queue(None, props, response_content)
         return JSONResponse(content=response_content, status_code=200)
 
     else:
-        # 티켓번호가 존재하지 않는 경우 오류 처리
-        print(f"Error: Ticket number {reserve_number} not found.")
-        response_content = {
-            "authSuccess": False,
-            "error": "Ticket number not found"
-        }
-        response_content.update(data)
-        print(f"Response: {response_content}")
-        await send_to_queue(None, props, response_content)
-        return JSONResponse(content=response_content, status_code=400)
+        if reserve_number in ticket_list_away['대표티켓번호'].values:
+            response_content = { "stateCode": "GEN-000", "authSuccess": True, "teamSide": "AWAY" }
+            response_content.update(data)
+            print(f"Response: {response_content}")
+            await send_to_queue(None, props, response_content)
+            return JSONResponse(content=response_content, status_code=200)
+        
+        else:
+            # 티켓번호가 존재하지 않는 경우 오류 처리
+            print(f"Error: Ticket number {reserve_number} not found.")
+            response_content = {
+                "stateCode": "GEN-002",
+                "authSuccess": False,
+                "teamSide": "NONE"
+            }
+            response_content.update(data)
+            print(f"Response: {response_content}")
+            await send_to_queue(None, props, response_content)
+            return JSONResponse(content=response_content, status_code=400)
+        
     
-  
